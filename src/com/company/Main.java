7b920ade388d9dfd0cba1a2a23c510281cf23a73
Olivia Chisman <olivia.chisman@gmail.com>
2018-10-01 23:14:22 -0500

    typo

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -16,7 +16,7 @@ public class Main {
      */
 
     public static void main(String[] args) throws IOException {
-        String fileName = "/Users/oliviachisman/dev/depaul/csc_421/convex-hull/TestFiles-Graham/case1.txt"
+        String fileName = "/Users/oliviachisman/dev/depaul/csc_421/convex-hull/TestFiles-Graham/case1.txt";
         BufferedReader reader = new BufferedReader(new FileReader(new File(fileName)));
 
         List<Point> points = new ArrayList<>();
